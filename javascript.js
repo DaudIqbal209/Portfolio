72b1858323d65b00ad19b8085770ed831ee1bee0
DaudIqbal209 <dawoodiqbal.528@gmail.com>
2021-10-28 11:05:17 +0500

    Portfolio 7

---
+++ javascript.js
@@ -0,0 +1,45 @@
+var nav = document.getElementById("menu");
+
+        nav.style.width = "0px";
+
+        function fun() {
+
+            if (nav.style.width == "0px") {
+
+                nav.style.width = "100%";
+
+            } else {
+
+                (nav.style.width = "0px")
+
+            }
+        }
+
+
+
+
+
+        $(document).ready(function() {
+          // Add smooth scrolling to all links
+          $("a").on('click', function(event) {
+
+              // Make sure this.hash has a value before overriding default behavior
+              if (this.hash !== "") {
+                  // Prevent default anchor click behavior
+                  event.preventDefault();
+
+                  // Store hash
+                  var hash = this.hash;
+
+                  // Using jQuery's animate() method to add smooth page scroll
+                  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                  $('html, body').animate({
+                      scrollTop: $(hash).offset().top
+                  }, 800, function() {
+
+                      // Add hash (#) to URL when done scrolling (default click behavior)
+                      window.location.hash = hash;
+                  });
+              } // End if
+          });
+      });
